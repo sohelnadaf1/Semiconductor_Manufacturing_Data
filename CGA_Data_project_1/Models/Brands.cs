480e6b37d163347899b770d8379e8f45de7ea6c1
sohelnadaf1 <sohel.nadaf@jkteck.com>
2022-08-03 20:46:36 +0530

    model added

---
+++ CGA_Data_project_1/Models/Brands.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CGA_Data_project_1.Models
+{
+    public class Brands
+    {
+        public int Id { get; set; }
+        [Required]
+        public string CompanyName { get; set; }
+
+        
+    }
+}
---
+++ CGA_Data_project_1/Models/CPU.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CGA_Data_project_1.Models
+{
+    public class CPU
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string ModelNumber { get; set; }
+        [Required]
+        public int Memory { get; set; }
+        [Required]
+        public int TotalCores { get; set; }
+        public int Transistor { get; set; }
+        public double Version { get; set; }
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+    }
+}
