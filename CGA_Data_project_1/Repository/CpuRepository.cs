b9f3dd7a3b984052ece29d07782b00a74e06e7f7
sohelnadaf1 <sohel.nadaf@jkteck.com>
2022-08-10 11:39:45 +0530

    Repository for CPU added and updated DB

---
+++ CGA_Data_project_1/Repository/CpuRepository.cs
@@ -0,0 +1,37 @@
+﻿using CGA_Data_project_1.Data;
+using CGA_Data_project_1.Models;
+using CGA_Data_project_1.Repository.RepoInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CGA_Data_project_1.Repository
+{
+    public class CpuRepository : ICpuRepository
+    {
+        private readonly AppDbContext _db;
+
+        public CpuRepository(AppDbContext appDb)
+        {
+            _db = appDb;
+        }
+        public List<CPU> GetAllCpus()
+        {
+            var data = _db.Cpus.Include(b => b.Brands).ToList();
+            return data;
+        }
+
+        public List<CPU> GetCPUByBrand(int id)
+        {
+            var data = _db.Cpus.Include(b => b.Brands).Where(c => c.BrandId == id).ToList();
+            return data;
+        }
+
+        public bool DExists(int id)
+        {
+            return _db.Cpus.Any(c => c.BrandId == id);
+        }
+    }
+}
---
+++ CGA_Data_project_1/Repository/RepoInterfaces/ICpuRepository.cs
@@ -0,0 +1,15 @@
+﻿using CGA_Data_project_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CGA_Data_project_1.Repository.RepoInterfaces
+{
+   public interface ICpuRepository
+    {
+        List<CPU> GetAllCpus();
+         List<CPU> GetCPUByBrand(int id);
+         bool DExists(int id);
+    }
+}
